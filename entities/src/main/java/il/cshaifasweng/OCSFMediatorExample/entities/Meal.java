98f0fb5132b7b48f8249c168c20c1b5b5c46f64d
samer <khsamer11@gmail.com>
2020-12-20 17:37:27 +0200

    i did commit

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Meal.java
@@ -0,0 +1,11 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import org.hibernate.annotations.Table;
+
+import javax.persistence.Entity;
+
+@Entity
+@Table(name="meal")
+public class Meal {
+
+}
