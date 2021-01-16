ee492d761a5a446a592b406e80741056bbf7cf62
akhateb <daliakhateb32@gmail.com>
2021-01-16 22:55:15 +0200

    complaint added to CustomerController configuration.addAnnotatedClass(Complaint.class); 3mltli moshkele haaaaaaaaaaaaay be2oli conflict

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Dao.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Dao.java
@@ -67,7 +67,6 @@ public class Dao<T> {
         configuration.addAnnotatedClass(Tablee.class);
         configuration.addAnnotatedClass(Booking.class);
         configuration.addAnnotatedClass(MealUpdate.class);
-       // configuration.addAnnotatedClass(Complaint.class);
 
 
         ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
